5814bdf7a368dda09a5f31927717cefe981a47ff
evanjt <evanjt@users.noreply.github.com>
2026-09-03 09:26:59 +0000

    deploy: 00c333fc6f2ed36cbce3f48fbfde648084da6f8c

--- 0.11.0/searcher-09f2665d.js
+++ 0.11.0/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-293eaaa6.js',
+                path_to_root + 'searchindex-a48c30fc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
